6ce6f1e2f96b27ca808a0411c2328a2b9af2387e
abhiroopdutta <abhiroopd@ymail.com>
2019-01-21 22:37:06 +0530

    read line

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # C-Shell
+
+A shell does three things in its lifetime - 
+    1.initialize - read and execute its configuration files
+    2.interpret - read commands from stdin and execute them
+    3.terminate - execute shutdown command, frees up memory and terminate 
+
+Our shell won't have any config files, no shutdown commands, so only looping
+to interpret instructions
+
+During the loop, the shell again does three things - 
+    Read: Read the command from standard input.
+    Parse: Separate the command string into a program and arguments.
+    Execute: Run the parsed command
--- main.c
+++ main.c
@@ -1,17 +1,9 @@
 #include<stdlib.h>
-
-/* A shell does three things in its lifetime - 
-    1.initialize - read and execute its configuration files
-    2.interpret - read commands from stdin and execute them
-    3.terminate - execute shutdown command, frees up memory and terminate 
-
-our shell won't have any config files, no shutdown commands, so only looping
-to interpret instructions
-*/
+#include<stdio.h> //for getline()
+#include <sys/types.h> //for ssize_t
 
 //argc is the number of command line arguments passed by the user
 //including the name of this program itself. argv contains the actual arguments
-
 int main(int argc, char **argv)
 {
 
@@ -20,5 +12,40 @@ int main(int argc, char **argv)
     //for portability to non POSIX systems use this macro, on POSIX systems value of this macro is 0
     //this macro is defined in stdlib.h
     return EXIT_SUCCESS; 
+
+}
+
+void shell_loop(void)
+{
+    //read the command, split it into program and argument, execute the program with arguments
+    char *line;
+    char **args;
+    int status;
+
+    //do while loop is used to get the value of the status variable 
+    do
+    {
+
+        printf("> ");
+        line = read_line();
+        args = split_line(line);
+        status = execute(args);
+
+        free(line);
+        free(args);
+
+    }while(status);
+
+}
+
+char *read_line(void)
+{
+
+    char *line = NULL;
+    ssize_t bufsize = 0; //ssize_t is signed size_t, to be able to hold -1 also.
+
+    //getline takes the address of the buffer, address of the variable that holds the size of the buffer
+    getline(&line, &bufsize, stdin);
+    return line;
 
 }
