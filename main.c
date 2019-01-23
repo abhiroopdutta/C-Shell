46d559e67abfc4568470e6da0f19d41fff3d8513
abhiroopdutta <abhiroopd@ymail.com>
2019-01-23 23:25:00 +0530

    split line

--- main.c
+++ main.c
@@ -1,18 +1,57 @@
 #include<stdlib.h>
 #include<stdio.h> //for getline()
 #include <sys/types.h> //for ssize_t
+#include <string.h>
+#define TOKEN_BUFSIZE 64
+#define TOKEN_DELIM " \t\r\n\a"
 
-//argc is the number of command line arguments passed by the user
-//including the name of this program itself. argv contains the actual arguments
-int main(int argc, char **argv)
+
+char *shell_read_line(void)
 {
+    char *line = NULL;
+    ssize_t bufsize = 0; //ssize_t is signed size_t, to be able to hold -1 also.
 
-    shell_loop(); //this will keep looping
+    //getline takes the address of the buffer, address of the variable that holds the size of the buffer
+    getline(&line, &bufsize, stdin);
+    return line;
+}
 
-    //for portability to non POSIX systems use this macro, on POSIX systems value of this macro is 0
-    //this macro is defined in stdlib.h
-    return EXIT_SUCCESS; 
+char **shell_split_line(char *line)
+{
+    int bufsize = TOKEN_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
 
+    if (!tokens)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, TOKEN_DELIM);
+
+    while(token!= NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+        if(position >= bufsize)
+        {
+            bufsize += TOKEN_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+
+            if(!tokens)
+            {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, TOKEN_DELIM);
+    }
+
+    tokens[position] = NULL;
+    return tokens;
 }
 
 void shell_loop(void)
@@ -27,9 +66,9 @@ void shell_loop(void)
     {
 
         printf("> ");
-        line = read_line();
-        args = split_line(line);
-        status = execute(args);
+        line = shell_read_line();
+        args = shell_split_line(line);
+        // status = shell_execute(args);
 
         free(line);
         free(args);
@@ -38,14 +77,15 @@ void shell_loop(void)
 
 }
 
-char *read_line(void)
+//argc is the number of command line arguments passed by the user
+//including the name of this program itself. argv contains the actual arguments
+int main(int argc, char **argv)
 {
 
-    char *line = NULL;
-    ssize_t bufsize = 0; //ssize_t is signed size_t, to be able to hold -1 also.
+    shell_loop(); //this will keep looping
 
-    //getline takes the address of the buffer, address of the variable that holds the size of the buffer
-    getline(&line, &bufsize, stdin);
-    return line;
+    //for portability to non POSIX systems use this macro, on POSIX systems value of this macro is 0
+    //this macro is defined in stdlib.h
+    return EXIT_SUCCESS; 
 
 }
